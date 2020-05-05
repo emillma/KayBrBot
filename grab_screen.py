9b351fc296a749e3b3d1488cdaac1ef0ca077f3b
Emil Martens <emil.martens@gmail.com>
2020-05-05 21:58:37 +0200

    Added working typingbot

---
+++ grab_screen.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 14:11:40 2020
+
+@author: emilm
+"""
+
+
+import time
+
+import cv2
+import mss
+import numpy
+import pytesseract
+import pyautogui
+import numpy as np
+import re
+
+
+pytesseract.pytesseract.tesseract_cmd = (
+    r'C:\Program Files\Tesseract-OCR\tesseract.exe')
+CONFIG = (
+    '-c tessedit_char_whitelist="C_ abcdefghijklmnopqrstuvwxyz" --psm 4')
+def textwriter(text):
+    for c in text:
+        yield pyautogui.write(c, interval=0)
+
+with mss.mss() as sct:
+    # Part of the screen to capture
+    monitor = {"top": 450, "left": 450, "width": 1100, "height": 125}
+
+    done = True
+    while "Screen capturing":
+        img = numpy.array(sct.grab(monitor))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if np.abs(img[45,25] - 232) > 20:
+            continue
+        cv2.imshow("OpenCV/Numpy normal", img)
+        if cv2.waitKey(100) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+
+        if done:
+            text = pytesseract.image_to_string(img, config = CONFIG)
+            text = text.replace('_', ' ')
+            text = text.replace('  ', ' ')
+            text = text.replace('   ', ' ')
+            text = text.replace('\n', '')
+
+            if (text[-17:] != 'Click to activate'
+                and np.count_nonzero(np.abs(img - 136) < 10) < 1000):
+                print(text)
+                pyautogui.write(text, interval=0.08)
+
+
+
