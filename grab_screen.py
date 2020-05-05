be2ffc868aa6a218dc56ce48e51d4487338c727b
Emil Martens <emil.martens@gmail.com>
2020-05-05 22:08:24 +0200

    Safety fix

--- grab_screen.py
+++ grab_screen.py
@@ -16,7 +16,7 @@ import pyautogui
 import numpy as np
 import re
 
-
+TYPETIME = 0.01
 pytesseract.pytesseract.tesseract_cmd = (
     r'C:\Program Files\Tesseract-OCR\tesseract.exe')
 CONFIG = (
@@ -26,30 +26,35 @@ def textwriter(text):
         yield pyautogui.write(c, interval=0)
 
 with mss.mss() as sct:
+    run = False
     # Part of the screen to capture
-    monitor = {"top": 450, "left": 450, "width": 1100, "height": 125}
+    monitor = {"top": 450, "left": 450, "width": 1100, "height": 155}
 
-    done = True
+
     while "Screen capturing":
         img = numpy.array(sct.grab(monitor))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("OpenCV/Numpy normal", img)
         if np.abs(img[45,25] - 232) > 20:
             continue
-        cv2.imshow("OpenCV/Numpy normal", img)
         if cv2.waitKey(100) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
+            break
 
-        if done:
-            text = pytesseract.image_to_string(img, config = CONFIG)
-            text = text.replace('_', ' ')
-            text = text.replace('  ', ' ')
-            text = text.replace('   ', ' ')
-            text = text.replace('\n', '')
+        text = pytesseract.image_to_string(img, config = CONFIG)
+        text = text.replace('_', ' ')
+        text = text.replace('  ', ' ')
+        text = text.replace('   ', ' ')
+        text = text.replace('\n', '')
 
-            if (text[-17:] != 'Click to activate'
-                and np.count_nonzero(np.abs(img - 136) < 10) < 1000):
-                print(text)
-                pyautogui.write(text, interval=0.08)
+        if (text[-17:] == 'Click to activate'
+            and np.count_nonzero(np.abs(img - 136) < 10) > 1000):
+            if not run:
+                print('ready to go')
+            run = True
+        elif run:
+            print(text)
+            pyautogui.write(text, interval=TYPETIME)
 
 
 
